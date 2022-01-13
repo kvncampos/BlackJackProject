9f331da39a4f07d6c8d6683de2c116d0489f4e72
kvncampos <89799176+kvncampos@users.noreply.github.com>
2022-01-13 17:09:36 -0600

    Fixed some Issue with Restarting Game

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def card_selector():
     return cards[card_pick1]
 
 
-# Function for Current Random Hand
+# Function for Random Hand Selection
 def blackjack(player):
     if player == "user":
         user = [(card_selector()), (card_selector())]
@@ -47,7 +47,6 @@ def card_score(deck):
                 for n, i in enumerate(user_cards):
                     if i == 11:
                         user_cards[n] = 1
-
         return deck_score
 
 
@@ -58,25 +57,14 @@ def draw_card(player):
         dealer_cards.append(card_selector())
 
 
-dealer_cards = blackjack("dealer")
-user_cards = blackjack("user")
-
-# Cards Drawn
-blackjack("user")
-blackjack("dealer")
-
-# Current Score
-card_score("dealer")
-card_score("user")
-
-
 # The actual BlackJack Game Function
 def blackjack_game():
     print(art.logo)
+
     flag = True
     while flag:
-        print(f"Dealer Drew: [{dealer_cards[0]}, _ ]")
-        print(f'You Drew: {user_cards}, Current Score: {card_score("user")}')
+        print(f"Dealer's Hand: [{dealer_cards[0]}, _ ]")
+        print(f'Your Hand: {user_cards}, Current Score: {card_score("user")}')
 
         if card_score("dealer") == 21:
             print(f"Your final hand: {user_cards}, Final Score: {card_score('user')}")
@@ -124,25 +112,49 @@ def blackjack_game():
         if user_choice == "y":
             draw_card("user")
             if card_score("user") > 21:
-                print("Player Bust. You Lose!\nYour Score went over: {card_score('user')}")
+                print("Player Bust. You Lose! Your Score went over.")
+                print(f"Your final hand: {user_cards}, Final Score: {card_score('user')}")
                 flag = False
 
             # else:
-                # print(f"Your current hand: {user_cards}, Current Score: {card_score('user')}")
+            # print(f"Your current hand: {user_cards}, Current Score: {card_score('user')}")
 
-    retry_game = input("Do you want to play a game of BlackJack? Type 'y' or 'n': ").lower().strip()
+    retry_game = input("Do you want to play again? Type 'y' or 'n': ").lower().strip()
     if retry_game == "n":
         flag = False
+        print("Thank you for visiting. Have a good day.")
         quit()
     else:
         os.system('cls' if os.name == 'nt' else 'clear')
+        # Cards Drawn
+        flag = False
+
+
+# Cards Drawn
+blackjack("user")
+blackjack("dealer")
+
+dealer_cards = blackjack("dealer")
+user_cards = blackjack("user")
+
+# # Current Score
+# card_score("dealer")
+# card_score("user")
+
+keep_playing = True
+while keep_playing:
+    blackjack("user")
+    blackjack("dealer")
+
+    dealer_cards = blackjack("dealer")
+    user_cards = blackjack("user")
+    # Intro to the Game Below
+    start_game = input("Do you want to play a Game of BlackJack? Type 'y' or 'n'. ").lower().strip()
+    if start_game == "y":
         blackjack_game()
 
+    else:
+        keep_playing = False
 
-# Intro to the Game Below
-start_game = input("Do you want to play a Game of BlackJack? Type 'y' or 'n'. ").lower().strip()
-if start_game == "y":
-    blackjack_game()
-else:
-    print("Thank you for visiting. Have a good day.")
-    quit()
+print("Thank you for visiting. Have a good day.")
+quit()
